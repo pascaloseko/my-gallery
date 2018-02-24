578a717e3ff90b560ae14dbf85b50aaf8c9d885c
cospamental <cospa18@gmail.com>
2018-02-24 22:11:27 +0300

    registered the app models

--- gallery/mygallery/admin.py
+++ gallery/mygallery/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Gallery,Location,Category
 
-# Register your models here.
+admin.site.register(Gallery)
+admin.site.register(Location)
+admin.site.register(Category)
