6182ab12a9fe1940200b16ea752d26b95f22c810
cospamental <cospa18@gmail.com>
2018-02-24 22:12:34 +0300

    provided tests for different app methods like saving of an image

--- gallery/mygallery/tests.py
+++ gallery/mygallery/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
-
+from .models import Gallery,Location,Category
 # Create your tests here.
+class GalleryTestClass(TestCase):
+
+      # Set up method
+      def setUp(self):
+            self.mygallery=Gallery(image_name='my awesome wb Gallery',description='brilliant and top of the pops')
+
+      # Testing  instance
+      def test_instance(self):
+            self.assertTrue(isinstance(self.mygallery,Gallery))
+
+      # Testing Save Method
+      def test_save_method(self):
+            self.mygallery.save_images()
+            gallery = Gallery.objects.all()
+            self.assertTrue(len(gallery) > 0)
