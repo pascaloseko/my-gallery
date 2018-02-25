fb429848e62137b82ddeb85307e7f3ee9e6376f9
cospamental <cospa18@gmail.com>
2018-02-25 23:41:37 +0300

    image mmethod that queries a single image bt id

--- gallery/mygallery/views.py
+++ gallery/mygallery/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render
+from django.http import HttpResponse,Http404
 from .models import Gallery,Category
+from django.db.models.base import ObjectDoesNotExist
 
 # Create your views here.
 def welcome(request):
       photos = Gallery.objects.all()
       return render(request, 'index.html',{"photos":photos})
+
+"""
+
+method that selects a single image and displays it
+
+"""
+def image(request, image_id):
+      try:
+            image = Gallery.objects.get(id = image_id)
+      except Gallery.DoesNotExist:
+            raise Http404()
+
+      return render(request,"image.html", {"image":image})
 
 def search_results(request):
       if 'category' in request.GET and request.GET("category"):
